5c19b746aeb5c965e16d9a58ae72ac2fe68a5a2c
Justin Palmer <justincharlespalmer@gmail.com>
2021-12-03 12:22:22 -0500

    sorted out input

--- LLClass.java
+++ LLClass.java
@@ -14,18 +14,36 @@
  *      hasNext
  * implement an exception that can be thrown when the class attempts an illegal operation
  * 
+ * 
+ * makes LL object from class just created
+ * demonstrate functionality of LL and iterator
+ * use each method at least once to
+ * handle exceptions
+ * make another LL object and let the user input a number of int values
+ * use the iterator to iterate over the values and sum them, printing the sum
  */
 
 import java.util.Scanner;
 import java.util.LinkedList;
 
-public class LLClass {
-    public static LinkedList<String> list = new LinkedList<String>();
+class LLClass {
+    public static LinkedList<Integer> list = new LinkedList<Integer>();
     public static Scanner sc = new Scanner(System.in);
     
+    public static void demo(LinkedList<Integer> list) {
+
+    }
+
     public static void main(String[] args) {
-        System.out.println(getInput());
-        sc.close();
+        System.out.println("Enter integer to LinkedList or -1 to exit");
+        System.out.printf(":> ");
+        int inp = getInput();
+        if (inp != -1) {
+            LLClass.list.add(inp);
+        } else {
+            inner.sum(LLClass.list);
+        }
+
     }
 
     public static int getInput() {
@@ -40,4 +58,12 @@ public class LLClass {
     }
 
 
+}
+
+class inner extends LLClass {
+    public static int sum = 0;
+    public static LinkedList<Integer> sum(LinkedList<Integer> list) {
+
+        return list;
+    }
 }
